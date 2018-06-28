4d3d1e38ad26762e374ee9c69653e90ad20e894b
Josh Clifford <josh@kazi.lan>
2018-06-29 00:53:24 +0200

    Modified Tests.

--- test/VSTeamBuilder.Tests.ps1
+++ test/VSTeamBuilder.Tests.ps1
@@ -121,9 +121,9 @@ Describe 'Get-TBToken' {
         True | Should Be True
     }
 }
-Describe 'Use-TBConnection' {
-    It 'Passes Use-TBConnection' {
-        Use-TBConnection -Paramater1 "test" -Paramater2 "test2" -Paramater3 "test3" -Paramater4 "test4"
+Describe 'Add-TBConnection' {
+    It 'Passes Add-TBConnection' {
+        Add-TBConnection -Paramater1 "test" -Paramater2 "test2" -Paramater3 "test3" -Paramater4 "test4"
         True | Should Be True
     }
 }
