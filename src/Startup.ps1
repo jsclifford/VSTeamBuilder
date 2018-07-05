c3fe2212415d59ea6a8630b379ec975f1d5658fd
jsclifford <37558619+jsclifford@users.noreply.github.com>
2018-07-06 00:06:59 +0200

    No longer need Startup script.

--- src/Startup.ps1
+++
@@ -1,9 +0,0 @@
-$binDir = (Join-Path $PSScriptRoot 'lib')
-
-# Load basic TFS client assemblies
-try{
-    Add-Type -Path (Join-Path $BinDir 'Microsoft.TeamFoundation.Common.dll') -Verbose
-    Add-Type -Path (Join-Path $BinDir 'Microsoft.TeamFoundation.Client.dll') -Verbose
-}catch{
-    Write-Verbose "Could not load DLLS $_"
-}
