56223a394cf188ff0bd905b73a96abf739e2e71e
jsclifford <josh@kazi.lan>
2018-08-18 16:39:34 +0200

    added appveyor build process.

--- build/build.ps1
+++ build/build.ps1
@@ -1,3 +1,4 @@
+Import-Module PSake
 
 # Builds the module by invoking psake on the build.psake.ps1 script.
-Invoke-PSake $PSScriptRoot\build.psake.ps1 -taskList Test -Verbose
+Invoke-PSake $PSScriptRoot\build.psake.ps1 -taskList Test
