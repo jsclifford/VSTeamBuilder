9d29cc37fdf2dfdc17d9aa4269142766bf66e8a6
jsclifford <josh@kazi.lan>
2018-08-19 01:29:36 +0200

    Updated installed

--- build/appveyor.install.ps1
+++ build/appveyor.install.ps1
@@ -3,6 +3,7 @@ Install-PackageProvider -Name NuGet -Force
 Write-Verbose -Message "PowerShell version $($PSVersionTable.PSVersion)" -Verbose
 $moduleData = Import-PowerShellDataFile "$($env:APPVEYOR_BUILD_FOLDER)\src\VSTeamBuilder.psd1"
 $moduleData.RequiredModules | ForEach-Object { Install-Module $PSItem.ModuleName -RequiredVersion $PSItem.ModuleVersion -Repository PSGallery -Scope CurrentUser -Force }
-Install-Module pester,psake,psscriptanalyzer,platyPS -Scope CurrentUser -Force
+Install-Module pester -SkipPublisherCheck -Force -Scope CurrentUser
+Install-Module psake,psscriptanalyzer,platyPS -Scope CurrentUser -Force
 
 Import-Module Pester,Psake,PSScriptAnalyzer
