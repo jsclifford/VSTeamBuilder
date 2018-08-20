f0f40a4f8545f876c280b7f8e96f96aa4181eaa1
jsclifford <josh@kazi.lan>
2018-08-21 01:32:23 +0200

    More updates.

--- build/buildenv.install.ps1
+++ build/buildenv.install.ps1
@@ -8,14 +8,13 @@ if($null -ne $env:APPVEYOR_BUILD_FOLDER){
 }else{
     $buildfolder = $Env:BUILD_SOURCESDIRECTORY
     Write-Verbose "This is an VSTS Build"
-    . $PSScriptRoot\buildenv.install-dotnet.ps1 -Version '2.0.0.0'
 }
 Install-PackageProvider -Name NuGet -Force -Scope CurrentUser
 Write-Verbose -Message "PowerShell version $($PSVersionTable.PSVersion)" -Verbose
 $moduleData = Import-PowerShellDataFile "$($buildfolder)\src\VSTeamBuilder.psd1"
 #$moduleData.RequiredModules | ForEach-Object { Install-Module $PSItem.ModuleName -RequiredVersion $PSItem.ModuleVersion -Repository PSGallery -Scope CurrentUser -Force -SkipPublisherCheck }
 $moduleData.RequiredModules | ForEach-Object { Install-Module $PSItem -Repository PSGallery -Scope CurrentUser -Force }
-Install-Module pester -SkipPublisherCheck -Force -Scope CurrentUser
+Install-Module pester -Force -Scope CurrentUser
 Install-Module psake,psscriptanalyzer,platyPS -Scope CurrentUser -Force
 
 Import-Module Pester,Psake,PSScriptAnalyzer
