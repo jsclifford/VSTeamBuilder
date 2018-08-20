13263f4072b6e453e851b514a3feadf65ef6f3bb
jsclifford <josh@kazi.lan>
2018-08-21 01:02:32 +0200

    Updated build install process.

--- build/buildenv.install.ps1
+++ build/buildenv.install.ps1
@@ -1,10 +1,13 @@
 #Installs all required modules for appveyor build.
 if($null -ne $env:APPVEYOR_BUILD_FOLDER){
     $buildfolder = $env:APPVEYOR_BUILD_FOLDER
+    Write-Verbose "This is an Appveyor Build"
 }else{
     $buildfolder = $Env:BUILD_SOURCESDIRECTORY
+    Write-Verbose "This is an VSTS Build"
+    . $PSScriptRoot\buildenv.install-dotnet.ps1 -Version '2.0.0.0'
 }
-Install-PackageProvider -Name NuGet -Force
+Install-PackageProvider -Name NuGet -Force -Scope CurrentUser
 Write-Verbose -Message "PowerShell version $($PSVersionTable.PSVersion)" -Verbose
 $moduleData = Import-PowerShellDataFile "$($buildfolder)\src\VSTeamBuilder.psd1"
 #$moduleData.RequiredModules | ForEach-Object { Install-Module $PSItem.ModuleName -RequiredVersion $PSItem.ModuleVersion -Repository PSGallery -Scope CurrentUser -Force -SkipPublisherCheck }
@@ -13,6 +16,4 @@ Install-Module pester -SkipPublisherCheck -Force -Scope CurrentUser
 Install-Module psake,psscriptanalyzer,platyPS -Scope CurrentUser -Force
 
 Import-Module Pester,Psake,PSScriptAnalyzer
-
- . $PSScriptRoot\buildenv.install-dotnet.ps1 -Version '2.0.0.0'
 
