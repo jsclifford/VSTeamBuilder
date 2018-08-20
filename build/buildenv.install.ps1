0d730fcbbba37a4ec95544ff8c9978fc3841450f
jsclifford <josh@kazi.lan>
2018-08-21 01:45:32 +0200

    updates to buildenv.install

--- build/buildenv.install.ps1
+++ build/buildenv.install.ps1
@@ -9,13 +9,24 @@ if($null -ne $env:APPVEYOR_BUILD_FOLDER){
     $buildfolder = $Env:BUILD_SOURCESDIRECTORY
     Write-Verbose "This is an VSTS Build"
 }
-Install-PackageProvider -Name NuGet -Force -Scope CurrentUser
+Install-PackageProvider -Name NuGet -Force -Scope CurrentUser -
 Write-Verbose -Message "PowerShell version $($PSVersionTable.PSVersion)" -Verbose
 $moduleData = Import-PowerShellDataFile "$($buildfolder)\src\VSTeamBuilder.psd1"
 #$moduleData.RequiredModules | ForEach-Object { Install-Module $PSItem.ModuleName -RequiredVersion $PSItem.ModuleVersion -Repository PSGallery -Scope CurrentUser -Force -SkipPublisherCheck }
 $moduleData.RequiredModules | ForEach-Object { Install-Module $PSItem -Repository PSGallery -Scope CurrentUser -Force }
 Install-Module pester -Force -Scope CurrentUser -SkipPublisherCheck
 Install-Module psake,psscriptanalyzer,platyPS -Scope CurrentUser -Force
+$seperator = @'
+
+------------------------------------
+
+'@
+
+Write-Verbose $seperator
+Write-Verbose "Getting current module versions."
+Get-InstalledModule
+
+Write-Verbose $seperator
 
 Import-Module Pester,Psake,PSScriptAnalyzer
 
