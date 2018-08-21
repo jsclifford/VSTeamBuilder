0dd64894082b4f01d57c35536d2eebede6d8e531
jsclifford <josh@kazi.lan>
2018-08-21 02:14:25 +0200

    Another try.

--- build/appveyor.psake.ps1
+++ build/appveyor.psake.ps1
@@ -1,7 +1,7 @@
 Import-Module PSake
 
 # Builds the module by invoking psake on the build.psake.ps1 script.
-Invoke-psake $PSScriptRoot\build.psake.ps1 -taskList Test -Verbose
+Invoke-psake $PSScriptRoot\build.psake.ps1 -taskList Build -Verbose
 
 if ($psake.build_success -eq $false){
     Add-AppveyorMessage -Message "Unit Test Failed"
