c08e0f876a3f5cbfcca80ecfcca44dda81cf606e
jsclifford <josh@kazi.lan>
2018-08-29 00:18:26 +0200

    Created default tests for build process bug checking.

--- build/build.psake.ps1
+++ build/build.psake.ps1
@@ -465,6 +465,61 @@ Task Test -depends Build -requiredVariables TestRootDir, ModuleName, CodeCoverag
     }
 }
 
+Task TestDefault -depends Build -requiredVariables TestRootDir, ModuleName, CodeCoverageEnabled, CodeCoverageFiles,CodeCoverageOutPutFile,CodeCoverageOutputFileFormat,PesterReportFolder  {
+    if (!(Get-Module Pester -ListAvailable)) {
+        "Pester module is not installed. Skipping $($psake.context.currentTaskName) task."
+        return
+    }
+
+    Import-Module Pester
+
+    try {
+        Microsoft.PowerShell.Management\Push-Location -LiteralPath "$TestRootDir\default"
+
+        if ($TestOutputFile) {
+            $testing = @{
+                OutputFile   = $TestOutputFile
+                OutputFormat = $TestOutputFormat
+                PassThru     = $true
+                Verbose      = $VerbosePreference
+            }
+        }
+        else {
+            $testing = @{
+                PassThru     = $true
+                Verbose      = $VerbosePreference
+            }
+        }
+
+        if(-not (Test-Path($PesterReportFolder))){
+            mkdir $PesterReportFolder
+        }
+
+        # To control the Pester code coverage, a boolean $CodeCoverageEnabled is used.
+        if ($CodeCoverageEnabled) {
+            $testing.CodeCoverage = $CodeCoverageFiles
+            $testing.CodeCoverageOutPutFile = $CodeCoverageOutPutFile
+            $testing.CodeCoverageOutputFileFormat = $CodeCoverageOutputFileFormat
+        }
+
+        $testResult = Invoke-Pester @testing
+
+        Assert -conditionToCheck (
+            $testResult.FailedCount -eq 0
+        ) -failureMessage "One or more Pester tests failed, build cannot continue."
+
+        if ($CodeCoverageEnabled) {
+            $testCoverage = [int]($testResult.CodeCoverage.NumberOfCommandsExecuted /
+                                  $testResult.CodeCoverage.NumberOfCommandsAnalyzed * 100)
+            "Pester code coverage on specified files: ${testCoverage}%"
+        }
+    }
+    finally {
+        Microsoft.PowerShell.Management\Pop-Location
+        Remove-Module $ModuleName -ErrorAction SilentlyContinue
+    }
+}
+
 Task Publish -depends Build, Test, BuildHelp, GenerateFileCatalog, BeforePublish, CorePublish, AfterPublish {
 }
 
