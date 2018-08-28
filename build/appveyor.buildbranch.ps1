98aea8c98b82c6786bf68d2052ee0ac73ff0386b
jsclifford <josh@kazi.lan>
2018-08-29 00:07:21 +0200

    Updated buildbranch script.

--- build/appveyor.buildbranch.ps1
+++ build/appveyor.buildbranch.ps1
@@ -9,7 +9,7 @@ $body = @{
     accountName="jsclifford"
     projectSlug="VSTeamBuilder"
     branch="experimental"
-    commitId="<Your_commit_id>"
+    commitId="e852cf1a0e71aa589b106ebd33a334d18b1550a4"
 }
 $body = $body | ConvertTo-Json
 
